05ad9bce249ae7020a5d02ac6f707e40008064b4
rarias1898 <rarias1898@ceuandalucia.es>
2024-09-25 20:23:34 +0200

    added ej2

--- Ej1/src/main/java/com/ej1/controller/PalindromoController.java
+++ Ej1/src/main/java/com/ej1/controller/PalindromoController.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PalindromoController {
-
+	
 	@GetMapping("/validar-palabra/{palindromo}")
 	public String validarPalindromo(@PathVariable String palindromo) {
 		
-		String palab = palindromo.substring(0, palindromo.length() -1);
-		
-		System.out.println(palab);
+		String mensaje = "La palabra " + palindromo;
 		
 		
 		return "Hola";
---
+++ Ej2/src/main/java/com/ej2/controller/ApiController.java
@@ -0,0 +1,38 @@
+package com.ej2.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ej2.models.Cliente;
+
+@RestController
+public class ApiController {
+
+	private List<Cliente> clientes = new ArrayList<>();
+	
+	public ApiController() {
+		Cliente c1 = new Cliente(1, "angel", "aaridom", "pass1");
+		Cliente c2 = new Cliente(2, "pepe", "pepipo", "pass2");
+		Cliente c3 = new Cliente(3, "paco", "maracar", "pass3");
+		Cliente c4 = new Cliente(4, "manolo", "tamarindo", "pass4");
+		
+		clientes.add(c1);
+		clientes.add(c2);
+		clientes.add(c3);
+		clientes.add(c4);
+	}
+	
+	@GetMapping("/cliente")
+	public List<Cliente> obtenerClientes() {
+		return clientes;
+	}
+	
+	@GetMapping("cliente/{id}")
+	public List<Cliente> obtenerCliente(@RequestParam int id) {
+		return clientes;
+	}
+}
