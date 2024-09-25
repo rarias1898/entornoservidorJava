334bb80ffcdb0d08faf288515470f9bdc33dfa58
Ángel Arias <angadominguez96@gmail.com>
2024-09-25 22:30:08 +0200

    Ej2 seccond endpoint done

--- Ej2/src/main/java/com/ej2/controller/ApiController.java
+++ Ej2/src/main/java/com/ej2/controller/ApiController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +33,7 @@ public class ApiController {
 	}
 	
 	@GetMapping("cliente/{username}")
-	public Cliente getClient(@RequestParam String username) {
+	public Cliente getClient(@PathVariable String username) {
 		
 		for (Cliente cliente : clientes) {
 			if(cliente.getUsername().equalsIgnoreCase(username)) {
