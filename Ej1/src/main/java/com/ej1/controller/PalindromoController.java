0f197984295703f9365e69db6ec006e0d13fcb7a
Ángel Arias <angadominguez96@gmail.com>
2024-09-26 03:08:54 +0200

    terminado Ej1

--- Ej1/src/main/java/com/ej1/controller/PalindromoController.java
+++ Ej1/src/main/java/com/ej1/controller/PalindromoController.java
@@ -9,10 +9,27 @@ public class PalindromoController {
 	
 	@GetMapping("/validar-palabra/{palindromo}")
 	public String validarPalindromo(@PathVariable String palindromo) {
-		
-		String mensaje = "La palabra " + palindromo;
-		
-		
-		return "Hola";
+
+		String palabra = palindromo.toLowerCase();
+		String mensaje;
+
+		if (esPalindromo(palabra)) {
+			mensaje = "La palabra " + palabra + " es un palindromo";
+		} else {
+			mensaje = "La palabra " + palabra + " NO es un palindromo";
+		}
+
+		return mensaje;
+	}
+
+	private boolean esPalindromo(String palabra) {
+		int longitud = palabra.length();
+
+		for (int i = 0; i < longitud / 2; i++) {
+			if (palabra.charAt(i) != palabra.charAt(longitud - i - 1)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
