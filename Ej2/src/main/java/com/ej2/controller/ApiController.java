ae710a1f768a45cc7329df224844aaa68a722193
Ángel Arias <angadominguez96@gmail.com>
2024-09-27 00:57:37 +0200

    terminado ejercicios del 2 al 7

--- Ej2/src/main/java/com/ej2/controller/ApiController.java
+++ Ej2/src/main/java/com/ej2/controller/ApiController.java
@@ -3,10 +3,10 @@ package com.ej2.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.ej2.models.Cliente;
 
@@ -40,7 +40,55 @@ public class ApiController {
 				return cliente;
 			}
 		}
-		
+		return null;
+	}
+
+	@PostMapping("/clientes")
+	public Cliente addClient(@RequestBody Cliente newClient) {
+
+		clientes.add(newClient);
+		return newClient;
+	}
+
+	@PutMapping("/cliente/{id}")
+	public Cliente updateClient(@PathVariable int id, @RequestBody Cliente updatedClient) {
+
+		for(Cliente cliente : clientes) {
+			if (cliente.getId() == id) {
+				cliente.setName(updatedClient.getName());
+				cliente.setUsername(updatedClient.getUsername());
+				cliente.setPassword(updatedClient.getPassword());
+
+				return cliente;
+			}
+		}
+		return null;
+	}
+
+	@DeleteMapping("/cliente/{id}")
+	public ResponseEntity<String> deleteClient(@PathVariable int id) {
+
+		for (Cliente cliente : clientes) {
+			if (cliente.getId() == id) {
+				clientes.remove(cliente);
+				return ResponseEntity.ok("Cliente eliminado");
+			}
+		}
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado");
+	}
+
+	@PatchMapping("/cliente/{id}")
+	public Cliente updateSmallClient(@PathVariable int id, @RequestBody Cliente updateClient) {
+
+		for (Cliente cliente : clientes) {
+			if (cliente.getId() == id) {
+				cliente.setName(updateClient.getName());
+
+				return cliente;
+			}
+		}
+
 		return null;
 	}
 }
