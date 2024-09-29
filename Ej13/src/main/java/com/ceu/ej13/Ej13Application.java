5b6600b5a95b09cda7a6a642e2da8240edc31180
Ángel Arias <angadominguez96@gmail.com>
2024-09-29 21:34:18 +0200

    ejercicio 13 iniciado

---
+++ Ej13/src/main/java/com/ceu/ej13/Ej13Application.java
@@ -0,0 +1,13 @@
+package com.ceu.ej13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej13Application.class, args);
+	}
+
+}
---
+++ Ej13/src/test/java/com/ceu/ej13/Ej13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceu.ej13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
