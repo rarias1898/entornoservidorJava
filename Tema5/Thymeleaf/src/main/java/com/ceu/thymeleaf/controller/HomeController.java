4f17c4dbdcf6b04b3a04c82c90833f297c6198eb
rarias1898 <rarias1898@ceuandalucia.es>
2025-01-31 16:52:39 +0100

    prueba

--- Tema5/Thymeleaf/src/main/java/com/ceu/thymeleaf/controller/HomeController.java
+++ Tema5/Thymeleaf/src/main/java/com/ceu/thymeleaf/controller/HomeController.java
@@ -7,6 +7,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ceu.thymeleaf.model.User;
 
 @Controller
 public class HomeController {
@@ -16,18 +20,25 @@ public class HomeController {
 		model.addAttribute("mensaje", "Bienvenido a Thymeleaf");
 		model.addAttribute("nombre", "Hola Angel");
 		model.addAttribute("role", "admin");
-		List<String> listaNombres = new ArrayList<>(Arrays.asList("Angel", "Pepe", "Maria", "Ana", "Paco"));
+		List<String> listaNombres = new ArrayList<>(Arrays.asList("Angel", "Pepe", "Maria", "Ana", "Paco", "Diego"));
 		model.addAttribute("nombres", listaNombres);
 		return "home";
 	}
 	
-	@GetMapping("/index.html")
+	@GetMapping("/index")
 	public String index() {
 		return "index";
 	}
 	
-	@GetMapping("/form.html")
-	public String form() {
+	@GetMapping("/form")
+	public String form(Model model) {
+		model.addAttribute("user", new User());
 		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String form(@ModelAttribute User user, Model model) {
+		model.addAttribute("user", user);
+		return "result";
 	}
 }
