e8e83feed3f472cc6a3e6f1023e62c67fcf329ef
aaridoms <angadominguez96@gmail.com>
2025-01-31 01:52:18 +0100

    Cambiado de directorio para empezar el tema5 con Thymeleaf

--- Tema5/Thymeleaf/src/main/java/com/ceu/thymeleaf/controller/HomeController.java
+++ Tema5/Thymeleaf/src/main/java/com/ceu/thymeleaf/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.ceu.thymeleaf.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +15,19 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("mensaje", "Bienvenido a Thymeleaf");
 		model.addAttribute("nombre", "Hola Angel");
+		model.addAttribute("role", "admin");
+		List<String> listaNombres = new ArrayList<>(Arrays.asList("Angel", "Pepe", "Maria", "Ana", "Paco"));
+		model.addAttribute("nombres", listaNombres);
 		return "home";
 	}
 	
 	@GetMapping("/index.html")
 	public String index() {
 		return "index";
-	} 
+	}
+	
+	@GetMapping("/form.html")
+	public String form() {
+		return "form";
+	}
 }
