c581a3cf7cb54594e917ade9ccb14684f617e931
aaridoms <angadominguez96@gmail.com>
2025-02-07 23:42:52 +0100

    Terminado hasta el ej8

--- Tema5/Ej8/src/main/java/com/ceu/ej8/controller/ViewController.java
+++ Tema5/Ej8/src/main/java/com/ceu/ej8/controller/ViewController.java
@@ -1,11 +1,13 @@
 package com.ceu.ej8.controller;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ceu.ej8.model.Usuario;
@@ -44,5 +46,11 @@ public class ViewController {
 	public String primerUsuarioDisponible(Model model) { 
 		model.addAttribute("usuario", service.getFirstDisponible());
 		return "primerUsuario";
+	}
+	
+	@GetMapping("/nuevo/{id}")
+	public String verUsuario(Model model, @PathVariable int id) {
+		model.addAttribute("usuario", service.getUsuarioById(id));
+		return "usuario-form";
 	}
 }
