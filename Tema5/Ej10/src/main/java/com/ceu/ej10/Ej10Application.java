2fdecb1a8f224379c92d60d457e7ff953f77566d
rarias1898 <rarias1898@ceuandalucia.es>
2025-02-14 16:45:33 +0100

    añadidolo el CommandLineRunner

--- Tema5/Ej10/src/main/java/com/ceu/ej10/Ej10Application.java
+++ Tema5/Ej10/src/main/java/com/ceu/ej10/Ej10Application.java
@@ -33,8 +33,8 @@ public class Ej10Application implements CommandLineRunner {
 		
 		oficinaService.addOficina(oficina1);
 		
-		empleado1.setOficina(oficina1);
-		System.out.println(empleado1);
+		// empleado1.setOficina(oficina1);
+		// System.out.println(empleado1);
 	}
 
 }
